2be400a22439d80678a4fee2c81d2fb1e8e33a6d
Doremamo <147545056+Nilou-Hutao@users.noreply.github.com>
2024-05-28 02:22:37 +0000

    Start

--- Exp_calculator.py
+++ Exp_calculator.py
@@ -1,1 +1,11 @@
-print('hello')
+def main():
+    EXP = ['n번째 레벨 달성시 필요경험치(1부터 시작)',\
+           0,1]
+    
+    cur_lv = input("현재 레벨: ")
+    cur_exp = input('현재 경험치: ')
+    
+
+
+if __name__ == 'main':
+    main()
